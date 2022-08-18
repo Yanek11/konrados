3c480a79f56621a2cc8f972995a1edea76be96f7
KOnrad K <1jan1@protonmail.com>
2022-08-18 15:09:20 +0200

    commit from VM variables file

---
+++ Very Useful Scripts.ps1
@@ -0,0 +1,16 @@
+# Enabling NAT via HyperV
+New-NetIPAddress -IPAddress 1.1.1.254 -PrefixLength 24 -InterfaceIndex 53
+New-NetNat -Name NATNetwork -InternalIPInterfaceAddressPrefix 1.1.1.0/24
+
+Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 3001 -Protocol TCP -InternalIPAddress "1.1.1.1" -InternalPort 3389 -NatName NATNetwork
+Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 5001 -Protocol TCP -InternalIPAddress "1.1.1.1" -InternalPort 5985 -NatName NATNetwork
+
+
+### Edit firewall rule scope with PowerShell ### ADDING MULTIPLE IPs
+Get-NetFirewallrule -DisplayName "*remote desktop*" # searching for a rule
+
+Get-NetFirewallrule -Name "RemoteDesktop-UserMode-In-TCP"
+# ading multiple IPs
+Get-NetFirewallrule -Name "RemoteDesktop-UserMode-In-TCP" |   Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress '89.66.64.239','146.70.85.194','194.110.114.98'
+
+Get-NetFirewallrule -Name "WinRM 8888" |   Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress '10.0.0.0/24'
