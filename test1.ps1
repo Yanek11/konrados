aaf41c323de0c95d7adf8697057860d4733fb25f
KOnrad K <1jan1@protonmail.com>
2022-08-18 12:42:04 +0200

    Yet another

--- test1.ps1
+++ test1.ps1
@@ -1,2 +1,11 @@
 git init
 git add .
+git commit -m 'first commit for shared repo on laptop'
+
+git branch -M main
+git remote add origin https://github.com/Yanek11/konrados.git
+git push -u origin main
+# change laptop
+git add .
+git commit
+git push
