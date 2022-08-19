e6fe4441562e68a7539393bcadec3bf531dd6f13
KOnrad K <1jan1@protonmail.com>
2022-08-19 10:25:36 +0200

    file10

--- DiskInfo.ps1
+++ DiskInfo.ps1
@@ -1,9 +1,9 @@
 ### PARAMETERS and commandlet binding
-function get-Diskinfo{
+function get-DiskInfo{
 [cmdletbinding()]
 param (
     [parameter(Mandatory=$true) ]
-[string]$ComputerName
+[string]$ComputerName,
 [string]$Drive='c:'
 )
 Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceId='$Drive'" -ComputerName $ComputerName |select SystemName,DeviceID,@{n='Size(GB)';e={$_.Size / 1GB -as [int]}},@{n='Free(GB)';e={$_.Freespace / 1GB -as [int]}}
