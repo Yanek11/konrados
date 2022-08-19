c784c9e77633e264dedadeae25bc2e19e5f13ded
KOnrad K <1jan1@protonmail.com>
2022-08-19 07:21:55 +0200

    file9

--- Simple Scripts and Functions 01.ps1
+++ Simple Scripts and Functions 01.ps1
@@ -1,3 +1,11 @@
 Get-CimInstance -ClassName win32_bios -ComputerName srv02,srv03
 
-Get-CimInstance -ClassName Win32_LogicalDisk -Filter "deviceid='c:'" -ComputerName 'localhost'|select SystemName,DeviceID,@
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter "deviceid='c:'" -ComputerName 'localhost','srv02','srv03'|select SystemName,DeviceID,@{n='Size(GB)';e={$_.Size / 1GB -as [int]}},@{n='Free(GB)';e={$_.Freespace / 1GB -as [int]}}
+
+
+### PARAMETERS
+param (
+[string]$ComputerName='srv02',
+[string]$Drive='c:'
+)
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceId='$Drive'" -ComputerName $ComputerName |select SystemName,DeviceID,@{n='Size(GB)';e={$_.Size / 1GB -as [int]}},@{n='Free(GB)';e={$_.Freespace / 1GB -as [int]}}
