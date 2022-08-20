4f14e43f83c15097a00424bcb64b11a0f7f8862d
KOnrad K <1jan1@protonmail.com>
2022-08-20 11:20:29 +0200

    file11

--- Functions 03 -.ps1
+++ Functions 03 -.ps1
@@ -19,9 +19,12 @@ if($Errorlog) {
 }
 process{
     foreach($c in $ComputerName){
-$os=Get-CimInstance -ComputerName $c -ClassName  Win32_SystemOperatingSystem ;
-$disk=Get-CimInstance -ComputerName $c -ClassName win32_logicaldisk -filter "DeviceId='c:'" <#|select SystemName,DeviceID,@{n='Size(GB)';e={$_.Size / 1GB -as [int]}},@{n='Free(GB)';e={$_.Freespace / 1GB -as [int]}}#>;
-($os).pscomputername,@{e={($disk).Size / 1GB -as [int]}},@{e={($disk).Freespace / 1GB -as [int]}}
+$os=Get-CimInstance -ComputerName $c -ClassName  Win32_SystemOperatingSystem |select @{n='OS';e={$_.Pscomputername}};
+$disk=Get-CimInstance -ComputerName $c -ClassName win32_logicaldisk -filter "DeviceId='c:'" |select DeviceID,@{n='Size(GB)';e={$_.Size / 1GB -as [int]}},@{n='Free(GB)';e={$_.Freespace / 1GB -as [int]}};
+$os, $disk
+
+
+#@{n='OS';e={($os).pscomputername}}, @{n='Disk Size';e={($disk).Size / 1GB -as [int]}},@{n='Free space';e=(($disk).Freespace / 1GB -as [int])}
 
     }
 }
