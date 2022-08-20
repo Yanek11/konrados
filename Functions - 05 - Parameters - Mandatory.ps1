f6d3f07baf01f813f08c75a78d44f1d4abfeae11
KOnrad K <1jan1@protonmail.com>
2022-08-20 16:22:16 +0200

    file12

---
+++ Functions - 05 - Parameters - Mandatory.ps1
@@ -0,0 +1,42 @@
+function get-compinfo1 {
+[CmdletBinding()]
+param(
+        # support for multiple computers
+        [Parameter(Mandatory=$True)]
+        [String[]]$ComputerName , 
+        # switch - turn on Error Logging
+        [Switch]$Errorlog,
+        [String]$LogFile='C:\temp\errorlog.txt'
+    
+)
+begin{
+
+if($Errorlog) {
+            Write-Verbose 'Error log is turned on'
+            } else { Write-Verbose 'Error log is turned OFF'
+            }
+            ForEach($c in $ComputerName) {
+                Write-Verbose "Computer: $c"
+            }
+
+
+}
+process{
+    foreach($c in $ComputerName){
+        $os=Get-CimInstance -ComputerName $c -ClassName  Win32_OperatingSystem 
+        $disk=Get-CimInstance -ComputerName $c -ClassName win32_logicaldisk -filter "DeviceId='c:'"
+
+            $Prop=[ordered]@{ # [ordered] added to odred a hash table
+                'Computername'=$c
+                'OS Name'=$os.Caption
+                'OS Build'=$os.BuildNumber
+                'FreeSpace'=$disk.FreeSpace / 1GB -as [int]
+                    }
+                    $obj=New-Object -TypeName pscustomobject -Property $Prop 
+                    Write-Output $obj
+
+    } 
+}
+End{}
+
+}
---
+++ Functions - Hash Tables Important.ps1
@@ -0,0 +1,45 @@
+# RESULT
+# Computername OS Name                                             OS Build FreeSpace
+#------------ -------                                             -------- ---------
+#srv01        Microsoft Windows Server 2019 Datacenter Evaluation 17763           25
+#srv02        Microsoft Windows Server 2019 Datacenter Evaluation 17763           28
+#srv03        Microsoft Windows Server 2019 Datacenter Evaluation 17763           28
+
+function get-compinfo1 {
+[CmdletBinding()]
+param(
+    [String[]]$ComputerName , # [String[]] indicates multiple arguments/computers
+    [Switch]$Errorlog,
+    [String]$logfile='C:\temp\errorlog.txt'
+)
+begin{
+
+if($Errorlog) {
+            Write-Verbose 'Error log is turned on'
+            } else { Write-Verbose 'Error log is turned OFF'
+            }
+            ForEach($c in $ComputerName) {
+                Write-Verbose "Computer: $c"
+            }
+
+
+}
+process{
+    foreach($c in $ComputerName){
+        $os=Get-CimInstance -ComputerName $c -ClassName  Win32_OperatingSystem 
+        $disk=Get-CimInstance -ComputerName $c -ClassName win32_logicaldisk -filter "DeviceId='c:'"
+
+            $Prop=[ordered]@{ # [ordered] added to odred a hash table
+                'Computername'=$c
+                'OS Name'=$os.Caption
+                'OS Build'=$os.BuildNumber
+                'FreeSpace'=$disk.FreeSpace / 1GB -as [int]
+                    }
+                    $obj=New-Object -TypeName pscustomobject -Property $Prop 
+                    Write-Output $obj
+
+    } 
+}
+End{}
+
+}
