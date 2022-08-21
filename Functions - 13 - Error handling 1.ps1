93d8ea602f55bae64e17ef1722f20cc305e9548c
KOnrad K <1jan1@protonmail.com>
2022-08-21 11:42:07 +0200

    file13

---
+++ Functions - 13 - Error handling 1.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+this is synopsis
+#>
+
+    function get-thelp 
+{
+            [CmdletBinding()] 
+            param(
+                
+            )
+                
+            begin{}
+            process{} 
+            End{}
+
+}
