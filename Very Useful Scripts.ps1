d46128dc1536dce39c26755367b8ae0299d088d4
KOnrad K <1jan1@protonmail.com>
2022-08-23 07:07:11 +0200

    file13

--- Very Useful Scripts.ps1
+++ Very Useful Scripts.ps1
@@ -1,29 +1,27 @@
-# finding CLASS NAME for get-CimInstance
+#1 finding CLASS NAME for get-CimInstance
 Get-CimClass -Namespace root/CIMV2 | where CimClassName -like win32*system*
 
-# Enabling NAT via HyperV
+#2 Enabling NAT via HyperV
 New-NetIPAddress -IPAddress 1.1.1.254 -PrefixLength 24 -InterfaceIndex 53
 New-NetNat -Name NATNetwork -InternalIPInterfaceAddressPrefix 1.1.1.0/24
-
 Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 3001 -Protocol TCP -InternalIPAddress "1.1.1.1" -InternalPort 3389 -NatName NATNetwork
 Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/24" -ExternalPort 5001 -Protocol TCP -InternalIPAddress "1.1.1.1" -InternalPort 5985 -NatName NATNetwork
 
-
-### Edit firewall rule scope with PowerShell ### ADDING MULTIPLE IPs
+#3  Edit firewall rule scope with PowerShell ### ADDING MULTIPLE IPs - FIREWALL
 Get-NetFirewallrule -DisplayName "*remote desktop*" # searching for a rule
-
 Get-NetFirewallrule -Name "RemoteDesktop-UserMode-In-TCP"
 # ading multiple IPs
 Get-NetFirewallrule -Name "RemoteDesktop-UserMode-In-TCP" |   Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress '89.66.64.239','146.70.85.194','194.110.114.98'
-
 Get-NetFirewallrule -Name "WinRM 8888" |   Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress '10.0.0.0/24'
 
+#4
 ### Both commands solve the same problem but Invoke is much more efficient
-#1
+    #1 - FAST
 invoke-command -ComputerName (Get-Content servers.txt) {}
-#2 - serial execition. if one of executions takes along time, the whole thing stops
+    #2 - SLOW serial execition. if one of executions takes along time, the whole thing stops
 foreach ($s in (get-content servers.txt)) {Invoke-Command -ComputerName $s {}}
 
+#5
 # showing variables
 dir variable:
 dir Variable:\? 
@@ -39,3 +37,19 @@ Errorlog                       False
 MyError                        {HRESULT 0x80070035,Microsoft.Management.Infrastructure.CimCmdlets.GetCimInstanceCommand}
 #>
 dir Variable:\IsWindows
+
+#6 TABLE VIEWS 
+<# get-process as example
+default view shows weird columns
+Handles  NPM(K)    PM(K)      WS(K)     CPU(s)     Id  SI ProcessName
+-------  ------    -----      -----     ------     --  -- -----------
+    267      16     5484      25200       0.98   6148   3 ApplicationFrameHost
+    396      29    58568     111848       4.09   1140   3 brave
+    176       9     2140       7720       0.06   2504   3 brave
+#>
+# commands below are not working 100% . taken from Jumpstart YT training
+get-process -name *bra* |sort  priority |ft -view priority
+get-process -name *bra* |sort  StartTime |ft -view StartTime| select name,StartTime
+get-process |gm
+
+#7 Write-out vs Write-Host
