29be972fe0e0b35757677f88d9322d0c2b4dfc81
KOnrad K <1jan1@protonmail.com>
2022-08-24 17:45:41 +0200

    file13

--- 24 Compinfo Savill.ps1
+++ 24 Compinfo Savill.ps1
@@ -16,6 +16,10 @@ Not very interesting
  'Total RAM' ="{0:N2} GB" -f ($win32CSOut.totalphysicalmemory/1GB -as [Int32]);
  0:N2  (0) - placeholder, colon (:) - formatting style, 2 - decimal places to retain
  letter N lets PowerShell know we want this to be formatted as a numeric value, and to include commas to separate the numbers.
+ https://stackoverflow.com/questions/24616806/powershell-display-files-size-as-kb-mb-or-gb
+ https://ss64.com/ps/syntax-f-operator.html
+ https://www.tachytelic.net/2019/10/thousands-separators-powershell/
+ https://devblogs.microsoft.com/scripting/use-powershell-and-conditional-formatting-to-format-numbers/
  #>
 function CompInfo {
  [cmdletbinding()]
@@ -40,10 +44,14 @@ foreach ($computername in $computers)
         Write-Debug "Finished querying $computername"
         $paramout = [ordered]@{'ComputerName'=$computername;
         'Total RAM' ="{0:N2} GB" -f ($win32CSOut.totalphysicalmemory/1GB -as [Int32]);
-        #'Memory (GB)'=$win32CSOut.totalphysicalmemory/1GB -as [Int32];
+        
         'Procs'=$win32CSOut.numberofprocessors;
         'Version'=$win32OSOut.version;
-        'Free RAM' ="{0:N0} MB" -f ($win32OSOut.freephysicalmemory) 
+        'Free RAM' ="{0:N0} MB" -f ($win32OSOut.freephysicalmemory) ;
+        
+        # not working correctly
+        #'Free RAM 2' =( Format-Bytes $win32OSOut.freephysicalmemory) 
+        #'Memory (GB)'=$win32CSOut.totalphysicalmemory/1GB -as [Int32];
         #'Free Memory (mb)'=$win32OSOut.freephysicalmemory;
         
     }
@@ -57,3 +65,60 @@ foreach ($computername in $computers)
     }
 }
 }
+
+
+<#
+Function Format-Bytes {
+    Param
+    (
+        [Parameter(
+            ValueFromPipeline = $true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [float]$number
+    )
+    Begin{
+        $sizes = 'KB','MB','GB','TB','PB'
+    }
+    Process {
+        # New for loop
+        for($x = 0;$x -lt $sizes.count; $x++){
+            if ($number -lt [int64]"1$($sizes[$x])"){
+                if ($x -eq 0){
+                    return "$number B"
+                } else {
+                    $num = $number / [int64]"1$($sizes[$x-1])"
+                    $num = "{0:N0}" -f $num
+                    return "$num $($sizes[$x-1])"
+                }
+            }
+        }
+        <# Original way
+        if ($number -lt 1KB) {
+            return "$number B"
+        } elseif ($number -lt 1MB) {
+            $number = $number / 1KB
+            $number = "{0:N2}" -f $number
+            return "$number KB"
+        } elseif ($number -lt 1GB) {
+            $number = $number / 1MB
+            $number = "{0:N2}" -f $number
+            return "$number MB"
+        } elseif ($number -lt 1TB) {
+            $number = $number / 1GB
+            $number = "{0:N2}" -f $number
+            return "$number GB"
+        } elseif ($number -lt 1PB) {
+            $number = $number / 1TB
+            $number = "{0:N2}" -f $number
+            return "$number TB"
+        } else {
+            $number = $number / 1PB
+            $number = "{0:N2}" -f $number
+            return "$number PB"
+        }
+        #>
+ #   }
+  #  End{}
+#}
+#>
