99f6898f63204c11497900cc54744129bfda02c6
Yanek11 <110762418+Yanek11@users.noreply.github.com>
2023-12-16 12:01:05 +0100

    ff

---
+++ DevOps/python_work/main.py
@@ -0,0 +1,13 @@
+calculation_to_units=(24*60*60)
+name_of_unit="minutes"
+# print("20 days are " + str(50) + " minutes")
+#print(f"20 days are {50} minutes")
+#print(f"20 days are {20*calculation_to_units} {name_of_unit}")
+#print(f"35 days are {35*calculation_to_units} {name_of_unit}")
+ 
+ 
+def days_to_units():
+    print(f"20 days are {20*calculation_to_units} {name_of_unit}")
+    print("All good")
+    
+days_to_units()
